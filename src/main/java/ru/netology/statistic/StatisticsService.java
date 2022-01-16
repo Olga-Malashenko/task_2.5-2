9f72a31690d84251910fca551db61f4baec32587
Olga Malashenko <olga_birjukova@mail.ru>
2022-01-16 04:43:57 +0300

    Files StatisticsService.java and StatisticsServiceTest.java fixed

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,17 +2,19 @@ package ru.netology.statistic;
 
 public class StatisticsService {
     /**
-     * Calculate index of max income
+     * Calculate max income
      *
      * @param incomes - array of incomes
-     * @return - index of first max value
+     * @return -  first max value
      */
     public long findMax(long[] incomes) {
-        long current_max_index = 0;
+//        long current_max_index = 0;
         long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
+        for (long income : incomes) {
+            if (current_max < income) {
                 current_max = income;
+            }
+        }
         return current_max;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void shouldFindMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
